2343d373e2b9b2b3953e4182036d1c0d2d2ba6c8
ICT <muhammadziyonps@gmail.com>
2023-05-04 17:13:13 +0500

    Hello!!

--- app/src/main/java/com/example/nurafshonpm/Activities/activities/activity/profile/BottomSheetActivity.kt
+++ app/src/main/java/com/example/nurafshonpm/Activities/activities/activity/profile/BottomSheetActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nurafshonpm.R
 
-class bottom_sheet_rating : AppCompatActivity() {
+class BottomSheetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_sheet_rating)
