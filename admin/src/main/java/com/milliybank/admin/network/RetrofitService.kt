6564a0a4b6abaa0ac44a2d8f770daab7bcbbd5bb
Alisher <101879695+Alisher19121994@users.noreply.github.com>
2023-05-15 19:27:39 +0500

    I ve recreated one ite

--- admin/src/main/java/com/milliybank/admin/network/RetrofitService.kt
+++ admin/src/main/java/com/milliybank/admin/network/RetrofitService.kt
@@ -1,6 +1,7 @@
 package com.milliybank.admin.network
 
 import com.milliybank.admin.homePage.modul.AdminHome
+import com.milliybank.admin.homePage.modul.AdminHomeItem
 import com.milliybank.admin.homePage.modul.PostData
 import retrofit2.Call
 import retrofit2.http.Body
@@ -12,7 +13,7 @@ interface RetrofitService {
     @Headers("Content-type:application/json")
 
     @POST("announcement")
-    fun postAnnouncement(@Body postData: PostData):Call<AdminHome>
+    fun postAnnouncement(@Body postData: PostData):Call<AdminHomeItem>
 
     @GET("announcement")
     fun getAnnouncement():Call<AdminHome>
